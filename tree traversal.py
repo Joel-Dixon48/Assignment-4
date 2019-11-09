08190ec5c260200da94d5ee125e88dc8ee2342c9
Joel-Dixon48 <57547985+Joel-Dixon48@users.noreply.github.com>
2019-11-08 23:05:12 -0500

    Add files via upload

--- tree traversal.py
+++ tree traversal.py
@@ -4,19 +4,34 @@ class TreeNode(object):
         self.val = x
         self.left = None
         self.right = None
+def creatTree(A,root,i,n):
+    if i<n:
+        tmp = TreeNode(A[i])
+        root = tmp
+        root.left = creatTree(A,root.left,2*i+1,n)
+        root.right = creatTree(A,root.right,2*i+2,n)
+    return root
 
+def inorderTraversal(root):
+        if root:
 
-class YourSolution(object):
-    def inorderTraversal(self, root):
-
-
-
-    def preorderTraversal(self, root):
+            if root.val != None:
+                print(root.val,end=" ")
+            inorderTraversal(root.left)
+            inorderTraversal(root.right)
+def preorderTraversal(root):
+        if root:
+            if root.val != None:
+                print(root.val,end=" ")
+            preorderTraversal(root.right)
+            preorderTraversal(root.left)
 #
 if __name__ == '__main__':
-    A = [1,2,3,4,5]
-    root = TreeNode(A[0])
-
-
+    A = [1,None,2,3]
+    root = None
+    root = creatTree(A,root,0,A.__len__())
+    inorderTraversal(root)
+    print(" ")
+    preorderTraversal(root)
 
 
